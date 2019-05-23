f7469fd223dc694e9b23b0993fe745b5d88e1c64
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-05-24 00:33:40 +0100

    Move console drawing code into its own module

--- web/index.js
+++ web/index.js
@@ -1,28 +1,12 @@
-import * as R from 'ramda'
-import { parsePuzzle, SAMPLE_PUZZLE_8x8, SAMPLE_PUZZLE_10x10 } from '../common/parsePuzzle'
+import { parsePuzzle, SAMPLE_PUZZLE_10x10 } from '../common/parsePuzzle'
 import { solve } from '../common/solve'
-import { drawSolution as drawSolutionSvg } from './svg'
-
-// TODO: move drawSolution in console/index.js to common area and use that
-const drawSolutionConsole = (puzzle, solution) => {
-  const xs = R.range(0, puzzle.width)
-  const ys = R.range(0, puzzle.height)
-  for (const y of ys) {
-    let line = ''
-    for (const x of xs) {
-      const cell = solution.find(cell => cell.coords.x === x && cell.coords.y === y)
-      line += cell.value
-    }
-    console.log(line)
-  }
-}
+import { drawSolution } from './svg'
 
 const solveButton = document.getElementById('solve')
 solveButton.addEventListener('click', () => {
   const puzzle = parsePuzzle(SAMPLE_PUZZLE_10x10)
   const solutions = solve(puzzle)
   solutions.forEach(solution => {
-    drawSolutionConsole(puzzle, solution)
-    drawSolutionSvg(puzzle, solution)
+    drawSolution(puzzle, solution)
   })
 })
