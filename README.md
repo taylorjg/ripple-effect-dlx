c5d0447e52e7de379762c0535f1c341e5896074a
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-05-28 20:14:29 +0100

    Expand readme to cover details of the DLX matrix

--- README.md
+++ README.md
@@ -1,11 +1,131 @@
 # Description
 
-This repo contains code to solve Ripple Effect puzzles using Donald E. Knuth's DLX.
+Solve Ripple Effect puzzles using Donald E. Knuth's DLX.
+
+## Details of the DLX Matrix
+
+> Most of the following description is copied from
+[RippleEffectDlx](https://github.com/taylorjg/RippleEffectDlx) with a few modifications.
+
+### Primary columns
+
+* One primary column per position (row/col) in the grid.
+These primary columns ensure that the grid is fully populated
+i.e. every position in the grid is filled exactly once.
+Total number of these columns: `width * height`.
+
+* One primary column for each value in each room.
+These primary columns ensure that every room is fully populated
+and that there are no duplicate values in a room. For example,
+a room with four cells must contain one each of 1, 2, 3 and 4.
+Total number of these columns: `width * height`.
+
+### Secondary columns
+
+Secondary columns are used to enforce the proximity constraints.
+There are four sets of secondary columns for each number from 1 to the largest room size.
+For example, if the largest room size is 5, there will be 4 x 5 = 20 sets of secondary columns.
+A set of four secondary columns represents the ripple of a placed digit in each direction - 
+up, down, left and right.
+I think of this as reserving space around the placed digit in each direction.
+There are
+four sets of secondary columns for placed 1s,
+four sets of secondary columns for placed 2s,
+etc.
+The number of columns in each set of secondary columns is `width * height`.
+
+This is best explained using an example. Say we have a 5x5 grid with
+position (0,0) at top left. Lets say that we have placed a 2 at (1,1):
+
+```
+-----
+-2---
+-----
+-----
+-----
+```
+
+The first set of secondary columns represents the position of the 2 itself plus
+the positions above that must not contain another 2 (one of these lies off the top of the grid):
+
+```
+-x---   (01000)
+-x---   (01000)
+-----   (00000)
+-----   (00000)
+-----   (00000)
+
+0100001000000000000000000
+```
+
+The second set of secondary columns represents the position of the 2 itself plus
+the positions below that must not contain another 2:
+
+```
+-----   (00000)
+-x---   (01000)
+-x---   (01000)
+-x---   (01000)
+-----   (00000)
+
+0000001000010000100000000
+```
+
+The third set of secondary columns represents the position of the 2 itself plus
+the positions to the left that must not contain another 2 (one of these lies off the left of the grid):
+
+```
+-----   (00000)
+xx---   (11000)
+-----   (00000)
+-----   (00000)
+-----   (00000)
+
+0000011000000000000000000
+```
+
+The fourth set of secondary columns represents the position of the 2 itself plus
+the positions to the right that must not contain another 2:
+
+```
+-----   (00000)
+-xxx-   (01110)
+-----   (00000)
+-----   (00000)
+-----   (00000)
+
+0000001110000000000000000
+```
+
+In any given row of the DLX matrix, which represents the placement of a single digit,
+only four sets of secondary columns are relevant.
+All other sets of secondary columns will contain 0s.
+Hence, the DLX matrix is very sparse.
+
+As an extremely simplified example, if we have a 3x3 grid where the largest room size is 2,
+then we will have 4 sets of secondary columns for 1s and 4 sets of secondary columns for 2s.
+So, our placement of a 2 at (1,1) on a 3x3 grid would have secondary columns like this:
+
+| U,D,L,R sets for 1s | U,D,L,R sets for 2s |
+| ------------------- | ------------------- |
+| 000000000 000000000 000000000 000000000 | 010010000 000010010 000110000 000011000 |
+
+The final number of columns in the DLX matrix is `(2 + 4 * largest room size) * width * height`.
+For example, an 8x8 puzzle with a largest room size of 5 will result in a DLX matrix with
+`(2 + 4 * 5) * width * height` = `22 * 64` = `1408` columns.
 
 # TODO
 
-* Add support for secondary columns to [dlxlib](https://github.com/taylorjg/dlxlibjs)
-  * _(I can't believe I didn't include this at the time)_
+* Stop using local copy of dlxlib
+  * Will do so when I publish dlxlib 2.0.0
+    * Main new feature is support for secondary columns which are
+required in order to solve this type of puzzle
+* Add checkbox re whether to animate the solution
+* Add slider to control speed of animation
+* Add more puzzles
+  * Use a simpler format
+  * Encode more puzzles
+* Responsive UI  
 
 # Screenshots
 
--- dlxlib-tmp/index.js
+++ dlxlib-tmp/index.js
@@ -1,150 +1,20 @@
-(function(g,f){typeof exports==='object'&&typeof module!=='undefined'?f(exports):typeof define==='function'&&define.amd?define(['exports'],f):(g=g||self,f(g.dlxlib={}));}(this,function(exports){'use strict';class DataObject {
-
-  constructor(listHeader, rowIndex) {
-    this.listHeader = listHeader;
-    this.rowIndex = rowIndex;
-    this.up = this;
-    this.down = this;
-    this.left = this;
-    this.right = this;
-    if (listHeader) {
-      listHeader.addDataObject(this);
-    }
-  }
-
-  appendToRow(dataObject) {
-    this.left.right = dataObject;
-    dataObject.right = this;
-    dataObject.left = this.left;
-    this.left = dataObject;
-  }
-
-  appendToColumn(dataObject) {
-    this.up.down = dataObject;
-    dataObject.down = this;
-    dataObject.up = this.up;
-    this.up = dataObject;
-  }
-
-  unlinkFromColumn() {
-    this.down.up = this.up;
-    this.up.down = this.down;
-  }
-
-  relinkIntoColumn() {
-    this.down.up = this;
-    this.up.down = this;
-  }
-
-  loopUp(fn) { this.loop(fn, 'up'); }
-  loopDown(fn) { this.loop(fn, 'down'); }
-  loopLeft(fn) { this.loop(fn, 'left'); }
-  loopRight(fn) { this.loop(fn, 'right'); }
-
-  loop(fn, propName) {
-    for (let next = this[propName]; next !== this; next = next[propName]) {
-      fn(next);
-    }
-  }
-}class ColumnObject extends DataObject {
-
-  constructor() {
-    super(null, -1);
-    this.previousColumnObject = this;
-    this.nextColumnObject = this;
-    this.numberOfRows = 0;
-  }
-
-  appendColumnHeader(columnObject) {
-    this.previousColumnObject.nextColumnObject = columnObject;
-    columnObject.nextColumnObject = this;
-    columnObject.previousColumnObject = this.previousColumnObject;
-    this.previousColumnObject = columnObject;
-  }
-
-  unlinkColumnHeader() {
-    this.nextColumnObject.previousColumnObject = this.previousColumnObject;
-    this.previousColumnObject.nextColumnObject = this.nextColumnObject;
-  }
-
-  relinkColumnHeader() {
-    this.nextColumnObject.previousColumnObject = this;
-    this.previousColumnObject.nextColumnObject = this;
-  }
-
-  addDataObject(dataObject) {
-    this.appendToColumn(dataObject);
-    this.numberOfRows++;
-  }
-
-  unlinkDataObject(dataObject) {
-    dataObject.unlinkFromColumn();
-    this.numberOfRows--;
-  }
-
-  relinkDataObject(dataObject) {
-    dataObject.relinkIntoColumn();
-    this.numberOfRows++;
-  }
-
-  loopNext(fn) {
-    for (let next = this.nextColumnObject; next !== this; next = next.nextColumnObject) {
-      fn(next);
-    }
-  }
-}const EventEmitter = require('events');
-
+!function(t,o){"object"==typeof exports&&"undefined"!=typeof module?o(exports):"function"==typeof define&&define.amd?define(["exports"],o):o((t=t||self).dlxlib={})}(this,function(t){"use strict";class o{constructor(t,o){this.listHeader=t,this.rowIndex=o,this.up=this,this.down=this,this.left=this,this.right=this,t&&t.addDataObject(this)}appendToRow(t){this.left.right=t,t.right=this,t.left=this.left,this.left=t}appendToColumn(t){this.up.down=t,t.down=this,t.up=this.up,this.up=t}unlinkFromColumn(){this.down.up=this.up,this.up.down=this.down}relinkIntoColumn(){this.down.up=this,this.up.down=this}loopUp(t){this.loop(t,"up")}loopDown(t){this.loop(t,"down")}loopLeft(t){this.loop(t,"left")}loopRight(t){this.loop(t,"right")}loop(t,o){for(let e=this[o];e!==this;e=e[o])t(e)}}class e extends o{constructor(){super(null,-1),this.previousColumnObject=this,this.nextColumnObject=this,this.numberOfRows=0}appendColumnHeader(t){this.previousColumnObject.nextColumnObject=t,t.nextColumnObject=this,t.previousColumnObject=this.previousColumnObject,this.previousColumnObject=t}unlinkColumnHeader(){this.nextColumnObject.previousColumnObject=this.previousColumnObject,this.previousColumnObject.nextColumnObject=this.nextColumnObject}relinkColumnHeader(){this.nextColumnObject.previousColumnObject=this,this.previousColumnObject.nextColumnObject=this}addDataObject(t){this.appendToColumn(t),this.numberOfRows++}unlinkDataObject(t){t.unlinkFromColumn(),this.numberOfRows--}relinkDataObject(t){t.relinkIntoColumn(),this.numberOfRows++}loopNext(t){for(let o=this.nextColumnObject;o!==this;o=o.nextColumnObject)t(o)}}const n=require("events"),i={numSolutions:Number.MAX_SAFE_INTEGER,numPrimaryColumns:Number.MAX_SAFE_INTEGER};
 /**
- * @typedef {number[]} PartialSolution The indices of the matrix rows that comprise a partial solution.
- */
-
+   * @typedef {number[]} PartialSolution The indices of the matrix rows that comprise a partial solution.
+   */
 /**
- * @typedef {number[]} Solution The indices of the matrix rows that comprise a complete solution.
- */
-
+   * @typedef {number[]} Solution The indices of the matrix rows that comprise a complete solution.
+   */
 /**
- * @typedef {*} MatrixValue Matrix values can be of any time. Anything truthy is treated as a 1. Anything falsy is treated as a 0.
- */
-
+   * @typedef {*} MatrixValue Matrix values can be of any time. Anything truthy is treated as a 1. Anything falsy is treated as a 0.
+   */
 /**
- * @typedef {MatrixValue[]} MatrixRow A matrix row is an array of {MatrixValue}.
- */
-
+   * @typedef {MatrixValue[]} MatrixRow A matrix row is an array of {MatrixValue}.
+   */
 /**
- * @typedef {MatrixRow[]} Matrix A matrix is an array of {MatrixRow}.
- */
-
+   * @typedef {MatrixRow[]} Matrix A matrix is an array of {MatrixRow}.
+   */
 /**
- * Solves the matrix and returns an array of solutions.
- * @param {Matrix} matrix The matrix to be solved.
- * @param {object} [options] Optional options object.
- * @param {number} options.numSolutions The number of solutions to be returned. By default, all solutions are returned.
- * @param {number} options.numPrimaryColumns The number of primary columns. By default, all columns are primary.
- *     Any remaining columns are considered to be secondary columns.
- * @returns {Solution[]} The solutions that were found.
- */
-const solve = (matrix, options) => new Dlx().solve(matrix, options);
-
-/**
- * Creates an ES2015 Generator object that can be used to iterate over the solutions to the matrix.
- * @param {Matrix} matrix The matrix to be solved.
- * @param {object} [options] Optional options object.
- * @param {number} options.numPrimaryColumns The number of primary columns. By default, all columns are primary.
- *     Any remaining columns are considered to be secondary columns.
- * @returns {IterableIterator.<number>} An ES2015 Generator object that can be used to iterate over the solutions.
- */
-const solutionGenerator = function* (matrix, options) {
-  yield* new Dlx().solutionGenerator(matrix, options);
-};
-
-const defaultOptions = {
-  numSolutions: Number.MAX_SAFE_INTEGER,
-  numPrimaryColumns: Number.MAX_SAFE_INTEGER
-};
-
-class Dlx extends EventEmitter {
-
-  /**
    * Solves the matrix and returns an array of solutions.
    * @param {Matrix} matrix The matrix to be solved.
    * @param {object} [options] Optional options object.
@@ -152,150 +22,22 @@ class Dlx extends EventEmitter {
    * @param {number} options.numPrimaryColumns The number of primary columns. By default, all columns are primary.
    *     Any remaining columns are considered to be secondary columns.
    * @returns {Solution[]} The solutions that were found.
-   */
-  solve(matrix, options) {
-    const actualOptions = Object.assign({}, defaultOptions, options);
-    if (!Number.isInteger(actualOptions.numSolutions)) {
-      throw new Error('options.numSolutions must be an integer')
-    }
-    if (actualOptions.numSolutions < 0) {
-      throw new Error(`options.numSolutions can't be negative - don't be silly`)
-    }
-    const generator = this.solutionGenerator(matrix, actualOptions);
-    const numSolutions = actualOptions.numSolutions;
-    const solutions = [];
-    for (let index = 0; index < numSolutions; index++) {
-      const iteratorResult = generator.next();
-      if (iteratorResult.done) break
-      solutions.push(iteratorResult.value);
-    }
-    return solutions
-  }
-
-  /**
-   * Creates an ES2015 Generator object that can be used to iterate over the solutions to the matrix.
-   * @param {Matrix} matrix The matrix to be solved.
-   * @param {object} [options] Optional options object.
-   * @param {number} options.numPrimaryColumns The number of primary columns. By default, all columns are primary.
-   *     Any remaining columns are considered to be secondary columns.
-   * @returns {IterableIterator.<number>} An ES2015 Generator object that can be used to iterate over the solutions.
-   */
-  * solutionGenerator(matrix, options) {
-    const actualOptions = Object.assign({}, defaultOptions, options);
-    if (!Number.isInteger(actualOptions.numPrimaryColumns)) {
-      throw new Error('options.numPrimaryColumns must be an integer')
-    }
-    if (actualOptions.numPrimaryColumns < 0) {
-      throw new Error(`options.numPrimaryColumns can't be negative - don't be silly`)
-    }
-    const root = buildInternalStructure(matrix, actualOptions.numPrimaryColumns);
-    const searchState = new SearchState(this, root);
-    yield* search(searchState);
-  }
-}
-
-const buildInternalStructure = (matrix, numPrimaryColumns) => {
-
-  numPrimaryColumns = numPrimaryColumns || (matrix[0] ? matrix[0].length : 0);
-
-  const root = new ColumnObject();
-  const colIndexToListHeader = new Map();
-
-  matrix.forEach((row, rowIndex) => {
-    let firstDataObjectInThisRow = null;
-    row.forEach((col, colIndex) => {
-      if (rowIndex === 0) {
-        const listHeader = new ColumnObject();
-        if (colIndex < numPrimaryColumns) {
-          root.appendColumnHeader(listHeader);
-        }
-        colIndexToListHeader.set(colIndex, listHeader);
-      }
-      if (col) {
-        const listHeader = colIndexToListHeader.get(colIndex);
-        const dataObject = new DataObject(listHeader, rowIndex);
-        if (firstDataObjectInThisRow)
-          firstDataObjectInThisRow.appendToRow(dataObject);
-        else
-          firstDataObjectInThisRow = dataObject;
-      }
-    });
-  });
-
-  return root
-};
-
-function* search(searchState) {
-
-  searchState.searchStep();
-
-  if (searchState.isEmpty()) {
-    if (searchState.currentSolution.length) {
-      searchState.solutionFound();
-      yield searchState.currentSolution.slice().sort();
-    }
-    return
-  }
-
-  const c = chooseColumnWithFewestRows(searchState);
-  coverColumn(c);
-  for (let r = c.down; r !== c; r = r.down) {
-    searchState.pushRowIndex(r.rowIndex);
-    r.loopRight(j => coverColumn(j.listHeader));
-    yield* search(searchState);
-    r.loopLeft(j => uncoverColumn(j.listHeader));
-    searchState.popRowIndex();
-  }
-  uncoverColumn(c);
-}
-
-const chooseColumnWithFewestRows = searchState => {
-  let chosenColumn = null;
-  searchState.root.loopNext(column => {
-    if (!chosenColumn || column.numberOfRows < chosenColumn.numberOfRows) {
-      chosenColumn = column;
-    }
-  });
-  return chosenColumn
-};
-
-const coverColumn = c => {
-  c.unlinkColumnHeader();
-  c.loopDown(i => i.loopRight(j => j.listHeader.unlinkDataObject(j)));
-};
-
-const uncoverColumn = c => {
-  c.loopUp(i => i.loopLeft(j => j.listHeader.relinkDataObject(j)));
-  c.relinkColumnHeader();
-};
-
-class SearchState {
-
-  constructor(dlx, root) {
-    this.dlx = dlx;
-    this.root = root;
-    this.currentSolution = [];
-    this.stepIndex = 0;
-    this.solutionIndex = 0;
-  }
-
-  isEmpty() {
-    return this.root.nextColumnObject === this.root
-  }
-
-  pushRowIndex(rowIndex) {
-    this.currentSolution.push(rowIndex);
-  }
-
-  popRowIndex() {
-    this.currentSolution.pop();
-  }
-
-  searchStep() {
-    this.dlx.emit('step', this.currentSolution, this.stepIndex++);
-  }
-
-  solutionFound() {
-    this.dlx.emit('solution', this.currentSolution, this.solutionIndex++);
-  }
-}exports.Dlx=Dlx;exports.solutionGenerator=solutionGenerator;exports.solve=solve;Object.defineProperty(exports,'__esModule',{value:true});}));
+   */class s extends n{
+/**
+     * Solves the matrix and returns an array of solutions.
+     * @param {Matrix} matrix The matrix to be solved.
+     * @param {object} [options] Optional options object.
+     * @param {number} options.numSolutions The number of solutions to be returned. By default, all solutions are returned.
+     * @param {number} options.numPrimaryColumns The number of primary columns. By default, all columns are primary.
+     *     Any remaining columns are considered to be secondary columns.
+     * @returns {Solution[]} The solutions that were found.
+     */
+solve(t,o){const e=Object.assign({},i,o);if(!Number.isInteger(e.numSolutions))throw new Error("options.numSolutions must be an integer");if(e.numSolutions<0)throw new Error("options.numSolutions can't be negative - don't be silly");const n=this.solutionGenerator(t,e),s=e.numSolutions,u=[];for(let t=0;t<s;t++){const t=n.next();if(t.done)break;u.push(t.value)}return u}
+/**
+     * Creates an ES2015 Generator object that can be used to iterate over the solutions to the matrix.
+     * @param {Matrix} matrix The matrix to be solved.
+     * @param {object} [options] Optional options object.
+     * @param {number} options.numPrimaryColumns The number of primary columns. By default, all columns are primary.
+     *     Any remaining columns are considered to be secondary columns.
+     * @returns {IterableIterator.<number>} An ES2015 Generator object that can be used to iterate over the solutions.
+     */*solutionGenerator(t,o){const e=Object.assign({},i,o);if(!Number.isInteger(e.numPrimaryColumns))throw new Error("options.numPrimaryColumns must be an integer");if(e.numPrimaryColumns<0)throw new Error("options.numPrimaryColumns can't be negative - don't be silly");const n=u(t,e.numPrimaryColumns),s=new p(this,n);yield*function*t(o){o.searchStep();if(o.isEmpty())return void(o.currentSolution.length&&(o.solutionFound(),yield o.currentSolution.slice().sort()));const e=l(o);r(e);for(let n=e.down;n!==e;n=n.down)o.pushRowIndex(n.rowIndex),n.loopRight(t=>r(t.listHeader)),yield*t(o),n.loopLeft(t=>h(t.listHeader)),o.popRowIndex();h(e)}(s)}}const u=(t,n)=>{n=n||(t[0]?t[0].length:0);const i=new e,s=new Map;return t.forEach((t,u)=>{let l=null;t.forEach((t,r)=>{if(0===u){const t=new e;r<n&&i.appendColumnHeader(t),s.set(r,t)}if(t){const t=s.get(r),e=new o(t,u);l?l.appendToRow(e):l=e}})}),i};const l=t=>{let o=null;return t.root.loopNext(t=>{(!o||t.numberOfRows<o.numberOfRows)&&(o=t)}),o},r=t=>{t.unlinkColumnHeader(),t.loopDown(t=>t.loopRight(t=>t.listHeader.unlinkDataObject(t)))},h=t=>{t.loopUp(t=>t.loopLeft(t=>t.listHeader.relinkDataObject(t))),t.relinkColumnHeader()};class p{constructor(t,o){this.dlx=t,this.root=o,this.currentSolution=[],this.stepIndex=0,this.solutionIndex=0}isEmpty(){return this.root.nextColumnObject===this.root}pushRowIndex(t){this.currentSolution.push(t)}popRowIndex(){this.currentSolution.pop()}searchStep(){this.dlx.emit("step",this.currentSolution,this.stepIndex++)}solutionFound(){this.dlx.emit("solution",this.currentSolution,this.solutionIndex++)}}t.Dlx=s,t.solutionGenerator=function*(t,o){yield*(new s).solutionGenerator(t,o)},t.solve=(t,o)=>(new s).solve(t,o),Object.defineProperty(t,"__esModule",{value:!0})});
